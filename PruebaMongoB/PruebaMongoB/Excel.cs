bad1e0bdc37a5ce7feb2714f1045857a9a44c290
Ammi Wang <89940842+AmmiW03@users.noreply.github.com>
2023-03-30 16:33:24 -0600

    Add files via upload

---
+++ PruebaMongoB/PruebaMongoB/Excel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MongoDB.Bson;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace PruebaMongoB
+{
+    public class Excel
+    {
+        public static List<BsonDocument> ReadExcel(string filePath)
+        {
+            List<BsonDocument> documents = new List<BsonDocument>();
+
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            IWorkbook workbook = new XSSFWorkbook(fs);
+
+            ISheet sheet = workbook.GetSheetAt(0);
+
+            int rowsCount = sheet.PhysicalNumberOfRows;
+
+            for (int i = 1; i < rowsCount; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if (row == null)
+                    continue;
+                int cellsCount = row.Cells.Count;
+                BsonDocument document = new BsonDocument();
+                for (int j = 0; j < cellsCount; j++)
+                {
+                    ICell cell = row.GetCell(j);
+                    if (cell == null)
+                        continue;
+                    string cellValue = cell.ToString();
+                    string cellHeader = sheet.GetRow(0).GetCell(j).ToString();
+                    document.Add(cellHeader, cellValue);
+                }
+                documents.Add(document);
+            }
+
+            return documents;
+        }
+    }
+}
---
+++ PruebaMongoB/PruebaMongoB/MongoDB.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaMongoB
+{
+    public class MongoDB
+    {
+        public static void Insertar(BsonDocument document)
+        {
+            try
+            {
+                MongoClient oClient = new MongoClient("mongodb://localhost:27017");
+
+                var oDatabase = oClient.GetDatabase("controlCursos");
+
+                var coleccion = oDatabase.GetCollection<BsonDocument>("datos");
+                coleccion.InsertOne(document);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
